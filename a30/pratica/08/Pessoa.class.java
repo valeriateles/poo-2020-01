4d989f84656f28620e0fc746e9cc1b4eda5bdd80
valeriateles <70611366+valeriateles@users.noreply.github.com>
2020-12-03 17:56:40 -0300

    Create Pessoa.class.java

---
+++ a30/pratica/08/Pessoa.class.java
@@ -0,0 +1,26 @@
+
+package pratica08;
+
+
+public class Pessoa {
+    private String nome;
+
+    public Pessoa(String nome) {
+        setNome(nome);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return getNome();
+    }
+    
+    
+}
